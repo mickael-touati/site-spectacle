27b07a692c7a32699bdae6cde2dc35aaa15e9047
Mickael Touati <mickael.touati@laplateforme.io>
2026-02-03 11:26:37 +0100

    maj

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lakers</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   <header class="logo_lakers">
+    <div>
+    <h1><img src="images/Los_Angeles_Lakers_logo.svg.png" alt="logo_lakers"></h1>
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a href="index.php" class="active">Accueil</a></li>
+            <li><a href="profil.php" class="active">Profil</a></li>
+            <li><a href="reservation-form.php" class="active">Creer une reservation de salle</a></li>
+            <li><a href="shedule.php" class="active">Reservation</a></li>
+            <li><a href="signin.php" class="active">Connexion</a></li>
+            <li><a href="signup.php" class="active">Inscription</a></li>
+        </ul>
+    </nav>
+    </header>
